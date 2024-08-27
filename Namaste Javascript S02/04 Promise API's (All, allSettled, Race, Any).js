ab147a40567621b23e1a390a90c2ddca266907a0
mamun-akand <mamun.crick25@gmail.com>
2024-08-28 02:51:20 +0600

    Promise Method

--- Namaste Javascript S02/04 Promise API's (All, allSettled, Race, Any).js
+++ Namaste Javascript S02/04 Promise API's (All, allSettled, Race, Any).js
@@ -1,15 +1,46 @@
-let promise1 = new Promise((resolve, reject) => {
+let p1 = new Promise((resolve, reject) => {
     setTimeout(function () {
         resolve("P1 Success");
     }, 3000);
 });
-let promise2 = new Promise((resolve, reject) => {
+let p2 = new Promise((resolve, reject) => {
     setTimeout(function () {
-        resolve("P2 Success");
+        // resolve("P2 Success");
+        reject("P2 Error");
     }, 1000);
 });
-let promise3 = new Promise((resolve, reject) => {
+let p3 = new Promise((resolve, reject) => {
     setTimeout(function () {
         resolve("P3 Success");
     }, 2000);
 });
+
+// Promise.all([p1, p2, p3])
+//     .then(function (values) {
+//         console.log(values);
+//     })
+//     .catch(function (err) {
+//         console.error(err);
+//     })
+
+// Promise.allSettled([p1, p2, p3])
+//     .then(function (values) {
+//         console.log(values);
+//     })
+
+// Promise.race([p1, p2, p3])
+//     .then(function (values) {
+//         console.log(values);
+//     })
+//     .catch(function (err) {
+//         console.error(err);
+//     })
+
+Promise.any([p1, p2, p3])
+    .then(function (values) {
+        console.log(values);
+    })
+    .catch(function (err) {
+        console.error(err);
+    })
+
