a187cbf105bfca2912992a3d20f1ca5ff08ce1a3
mamun-akand <mamun.crick25@gmail.com>
2024-08-30 04:27:38 +0600

    Call-Bind-Apply

---
+++ Namaste Javascript S02/10 Call-Apply-Bind.js
@@ -0,0 +1,43 @@
+// call : function borrowing/sharing
+
+//example-1
+const student1 = {
+    name: "Liza",
+    printName: function () {
+        console.log(this.name);
+    }
+}
+const student2 = {
+    name: "Mamun",
+}
+student1.printName.call(student2); // call method
+
+//example-2
+const name1 = {
+    firstName: "Esraat",
+    lastName: "Zahan"
+}
+const name2 = {
+    firstName: "Mamun",
+    lastName: "Akand"
+}
+const printFullName = function () {
+    console.log(this.firstName + " " + this.lastName);
+}
+const printNameDistrict = function (vill, district) {
+    console.log(this.firstName + " " + this.lastName + " From " + vill + " " + district);
+}
+
+printFullName.call(name1);  //call method
+printFullName.call(name2);  //call method
+
+printNameDistrict.call(name2, "Alinapara", "Sherpur");                          // Call method
+//Apply Method: Only diff is second argument is Array-list.
+printNameDistrict.apply(name2, ["Alinapara", "Sherpur"]);                       // Apply method
+
+// Bind Method: function and bind together. then returns a new function.
+const returnedFunction = printNameDistrict.bind(name2, "Alinapara", "Sherpur"); // Bind method
+console.log(returnedFunction);
+returnedFunction();
+
+
